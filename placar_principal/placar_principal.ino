f9188b38dd89b11628e099dd344d2a559a78cbc8
almirmv <76816905+almirmv@users.noreply.github.com>
2024-11-20 12:47:28 -0300

    Inicio do projeto
    Esqueleto do projeto inicial

---
+++ placar_principal/placar_principal.ino
@@ -0,0 +1,17 @@
+/************************************************************
+PLACAR PARA CONTROLE DE PONTUAÇÃO DE JOGOS
+dev: Almir M Ventura, data inicio: 19/11/2024
+funcionamento: Placar principal recebe comandos por um controle remoto
+rf433MHz para aumentar ou reduriz pontuação. Marcador possui placar para
+dois times. Este placar feito com EESP8266 possui wifi e a pontuação pode
+ser visualizada por celular ou pelo receptor personalizado com tela oled.
+Placar principal possui botoes fisicos para aumentar ou reduzir pontuação.
+************************************************************/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
---
+++ placar_receptor/placar_receptor.ino
@@ -0,0 +1,21 @@
+/***************************************************************************
+PLACAR PARA CONTROLE DE PONTUAÇÃO DE JOGOS
+dev: Almir M Ventura, data inicio: 19/11/2024
+DESCRIÇÃO:
+Receptor WIFI para o placar principal. Possui tela 0.96 polegadas SD1306 de OLED,
+sem retroiluminacao, conectada nos pinos SDA (D6/GPIO14) e SCL (D5/GPIO12).
+Conecta pelo wifi no placar principal, puxar os dados e mostra no visor.
+Bibliotecas:
+* display: https://github.com/ThingPulse/esp8266-oled-ssd1306
+***************************************************************************/
+
+// SETUP ====================================================
+void setup(){
+ 
+}
+
+
+// LOOP ====================================================
+void loop(){
+
+}
